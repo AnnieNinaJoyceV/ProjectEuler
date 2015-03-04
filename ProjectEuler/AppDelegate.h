db35d59d67cae74547b36ba184d0b6f542868bdf
JEMS <JEMS-Macbook-Pro@shaili-solutions-MacPro3s-MacBook-Pro.local>
2015-03-04 16:19:55 +0530

    Initial Commit

---
+++ ProjectEuler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectEuler
+//
+//  Created by JEMS on 04/03/15.
+//  Copyright (c) 2015 Learn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectEuler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectEuler
+//
+//  Created by JEMS on 04/03/15.
+//  Copyright (c) 2015 Learn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
